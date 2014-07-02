be0e6fa3c0291d245c21c87f45276107d8dd7a27
triestpa <triestpa@grinnell.edu>
2014-07-02 17:37:33 -0400

    initialize and set up parse

--- gen/com/triestpa/ding/R.java
+++ gen/com/triestpa/ding/R.java
@@ -1271,6 +1271,8 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a0011;
         public static final int app_name=0x7f0a000d;
+        public static final int parse_app_id=0x7f0a0012;
+        public static final int parse_client_id=0x7f0a0013;
         public static final int title_section1=0x7f0a000e;
         public static final int title_section2=0x7f0a000f;
         public static final int title_section3=0x7f0a0010;
